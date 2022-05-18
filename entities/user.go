bd46cdf4a54a2983bfd569a09fc56b869e169a43
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-05-18 12:28:57 -0700

    create user entity

---
+++ entities/user.go
@@ -0,0 +1,9 @@
+package entities
+
+type User struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Roles    []Role `json:"roles"`
+}
