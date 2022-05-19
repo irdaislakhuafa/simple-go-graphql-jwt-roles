98a434bd85c3eee4a06afe3aba3361d42b1101bb
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-05-19 11:59:04 -0700

    add method to generate jwt.Token with claims

--- services/jwt.go
+++ services/jwt.go
@@ -2,10 +2,33 @@ package services
 
 import (
 	"context"
+	"log"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/irdaislakhuafa/simple-go-graphql-jwt-roles/entities"
 )
 
-func GenerateJwtToken(ctx context.Context, user *entities.User) {
+type TokenClaims struct {
+	UserId string   `json:"user_id"`
+	Roles  []string `json:"roles"`
+	jwt.StandardClaims
+}
 
+// mwthod to generate jwt.Token
+func GenerateJwtTokenWithClaims(ctx context.Context, user *entities.User) *jwt.Token {
+	log.Println("entering method to generate jwt.Token with claims")
+	jwtToken := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		&TokenClaims{
+			UserId: user.ID,
+			Roles: func() (roles []string) {
+				for _, v := range user.Roles {
+					roles = append(roles, v.Name)
+				}
+				return roles
+			}(),
+		},
+	)
+	log.Println("success generate jwt.Token with claims")
+	return jwtToken
 }
