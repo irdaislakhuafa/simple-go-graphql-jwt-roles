bba3339c8aab341f420c1375d4ce65d3ca9f2a8c
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-05-19 11:04:18 -0700

    initialize jwt.go

---
+++ services/jwt.go
@@ -0,0 +1,11 @@
+package services
+
+import (
+	"context"
+
+	"github.com/irdaislakhuafa/simple-go-graphql-jwt-roles/entities"
+)
+
+func GenerateJwtToken(ctx context.Context, user *entities.User) {
+
+}
